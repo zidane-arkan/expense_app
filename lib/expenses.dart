013d1f6b009f423a2044f462ca21859863ba411e
Zidane Arkan <zidark897@gmail.com>
2024-04-08 16:00:03 +0700

    add showModalBottomSheet

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -28,12 +28,33 @@ class _ExpensesState extends State<Expenses>{
     ),
   ];
 
+  void _openAddExpense() {
+    // When you in class that extends State, flutter automatically adds a context property
+    showModalBottomSheet(
+      // Context : metadata collection managed by flutter, so in here holds information about widget ex: expenses
+      // and The location in the tree where this widget builds.
+      context: context,
+      // If you see builder means must provide a function as a value
+      builder: (ctx)  => const Text("This is Add Expense Overlay"),
+    );
+  }
   @override  
   Widget build(BuildContext context){
     return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
       home: Scaffold(
+        appBar: AppBar(
+          title : const Text("Expense Tracker App"),
+          backgroundColor: Colors.purple,
+          actions: [
+            IconButton(
+              onPressed: _openAddExpense, 
+              icon: const Icon(Icons.add_rounded),
+              color: Colors.white,
+            )
+          ],
+        ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             const Text(
               "The Chart"
