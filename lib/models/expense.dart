e1fc77be9122cacf9b99d26bd8e5370cf14c3ec0
Zidane Arkan <zidark897@gmail.com>
2024-04-16 19:44:19 +0700

    final Category category;

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -40,5 +40,28 @@ class Expense {
 }
 
 class ExpenseBucket{
-  
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  // Alternatif constructor function
+  ExpenseBucket.forCategory(
+    List<Expense> allExpenses, this.category) 
+      : expenses = allExpenses
+        .where((expense) => expense.category == category)
+        .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get getTotalExpenses{
+    double sum = 0;
+
+    for(final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
+  }
 }
